7fa11fc2a3a2ba02e6285d9e828973e7108b2f25
Andy <alipinski@mail.com>
2022-10-16 14:19:56 +0200

    loading state, error handling

--- src/components/GithubUsers.js
+++ src/components/GithubUsers.js
@@ -4,12 +4,28 @@ const url = 'https://api.github.com/users';
 
 const GithubUsers = () => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const getUsers = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.log(data);
-    setUsers(data);
+    setIsLoading(true);
+    setError(false);
+
+    try {
+      const response = await fetch(url);
+      console.log(response);
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      const data = await response.json();
+
+      setUsers(data);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(error.message);
+      setError(true);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -23,23 +39,33 @@ const GithubUsers = () => {
           <h1 className='--text-center --text-light'>Github Users</h1>
           <div className='--line'></div>
         </header>
+        {isLoading && (
+          <div className='--center-all --p'>
+            <h2 className='--text-light'>Loading...</h2>
+          </div>
+        )}
+
         <div className='--grid-25 --py'>
-          {users.map((user) => {
-            const { avatar_url, login, id, html_url } = user;
-            return (
-              <div key={id} className='--card --bg-light --p --flex-start '>
-                <img
-                  src={avatar_url}
-                  alt='image'
-                  className='--profile-img --mx'
-                />
-                <span>
-                  <h4> {login}</h4>
-                  <a href={html_url}>View profile</a>
-                </span>
-              </div>
-            );
-          })}
+          {error ? (
+            <h4 className='--text-light'>Something went wrong...</h4>
+          ) : (
+            users.map((user) => {
+              const { avatar_url, login, id, html_url } = user;
+              return (
+                <div key={id} className='--card --bg-light --p --flex-start '>
+                  <img
+                    src={avatar_url}
+                    alt='image'
+                    className='--profile-img --mx'
+                  />
+                  <span>
+                    <h4> {login}</h4>
+                    <a href={html_url}>View profile</a>
+                  </span>
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
     </div>
